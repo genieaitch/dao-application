31d110cd5699f28519da341a5d05c73a3b5f7e05
genieaitch <hah4815@naver.com>
2025-01-21 11:48:42 +0900

    JPA DAO USER SAVE

---
+++ src/test/java/edu/kh/com/daoappication/DaoAppicationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kh.com.daoappication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoAppicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
