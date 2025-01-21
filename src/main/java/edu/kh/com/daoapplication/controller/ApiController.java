e1f058fd99245a31d87b68332e06955aa85b42b4
genieaitch <hah4815@naver.com>
2025-01-21 12:33:03 +0900

    JPA DAO PRODUCT get post

--- src/main/java/edu/kh/com/daoapplication/controller/ApiController.java
+++ src/main/java/edu/kh/com/daoapplication/controller/ApiController.java
@@ -6,15 +6,13 @@ import edu.kh.com.daoapplication.service.KHTProductService;
 import edu.kh.com.daoapplication.service.KHTUserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Slf4j
 @RestController
+@RequestMapping("/api") //맨 앞에 공통으로 들어갈 url api 명칭 작성
 public class ApiController {
 
     @Autowired
@@ -23,24 +21,24 @@ public class ApiController {
     @Autowired
     private KHTProductService khtProductService;
 
-    @GetMapping("/api/users")
+    @GetMapping("/users") // /api/users
     public List<KHTUser> findAllUsers() {
         List<KHTUser> users = khtUserService.findAll();
         log.info("findAll users: {}", users);
         return users;
     }
 
-    @PostMapping("/api/save")
+    @PostMapping("/save") // /api/save
     public KHTUser saveUser(@RequestBody KHTUser khtUser) {
         return khtUserService.save(khtUser);
     }
 
-    @GetMapping("/api/products")
+    @GetMapping("/products") // /api/products
     public List<KHTProduct> findAllProducts() {
         return khtProductService.findAll();
     }
 
-    @PostMapping("/api/saveProduct")
+    @PostMapping("/saveProduct") // /api/saveProduct
     public KHTProduct saveProduct(@RequestBody KHTProduct khtProduct) {
         return khtProductService.save(khtProduct);
     }
